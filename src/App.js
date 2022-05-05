6c2b74157fc86aab3856057ecc6a0881be321af4
GalMoussan <galmoussan@gmail.com>
2022-05-05 14:10:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './pages/Welcome';
+import ChoicePage from './pages/ChoicePage';
+import DrawPage from './pages/DrawPage';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom'
+import Waiting from './pages/Waiting';
+import  AppRoutes from './types/Routes'
+import GuessPage from './pages/GuessPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Header/>
+      </div>
+
+      {/* Routes for the entire app */}
+      <Routes>
+        <Route path={AppRoutes.Login} element={<LoginPage />} />
+        <Route path={AppRoutes.Welcome} element={<Welcome />} />
+        <Route path={AppRoutes.Choice} element={<ChoicePage />} />
+        <Route path={AppRoutes.Draw} element={<DrawPage />} />
+        <Route path={AppRoutes.Wating} element={<Waiting />} />
+        <Route path={AppRoutes.Guess} element={<GuessPage />} />
+      </Routes>
     </div>
   );
 }
